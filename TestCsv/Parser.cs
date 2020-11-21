d291d1f532a8e6c4d8e1eaf07f2935c1628e596f
jbronxxx <jbronxxx@gmail.com>
2020-11-21 19:47:05 +0500

    Добавьте файлы проекта.

---
+++ TestCsv/Parser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace TestCsv
+{
+    public class Parser
+    {
+        internal List<string> _csv { get; set; }
+
+        private List<string> _header = new List<string>();
+
+        public List<string> ReadCsv(string _csvPath)
+        {
+            _csv = File.ReadAllLines(_csvPath).ToList();
+
+            foreach (var lines in _csv)
+            {
+                Console.WriteLine(lines);
+            }
+
+            Console.WriteLine("\nThe file has been created.\n");
+
+            for (int i = 0; i <_csv.Count; i++)
+            {
+                Console.WriteLine($"Line {i + 1}:\n {_csv[i]}\n");
+            }
+
+            return _csv;
+        }
+
+        public List<string> CreateHeader(List<string> _csv)
+        {
+            string[] split = _csv[0].Split(',').ToArray();
+            
+            for (int i = 0; i < split.Length; i++)
+            {
+                _header.Add(split[i]);
+            }
+
+            Console.WriteLine("\nThe header has been created.\n");
+
+            for (int i = 0; i < _header.Count; i++)
+            {
+                Console.WriteLine($"Field {i + 1}: {_header[i]}\n");
+            }
+
+            return _header;
+        }
+    }
+}
---
+++ TestCsv/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+
+            parser.ReadCsv("");
+
+            var readCsv = parser.CreateHeader(parser._csv);
+
+            Console.WriteLine(readCsv.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
